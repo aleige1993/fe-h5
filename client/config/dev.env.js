8cc1295e3e67e4d7f6c8f99aa2db482a49cc14a1
肖洒 <xiaos@songcw.com>
2018-11-19 09:35:49 +0800

    车辆详情-购车方案绑数据

--- client/config/dev.env.js
+++ client/config/dev.env.js
@@ -9,6 +9,6 @@ module.exports = merge(prodEnv, {
   XHRLOG: true, // 是否console出ajax的详细信息；右上角的Notice是否打印url和code
   HTTPBASEURL: '"https://feh5-test.songchedai.com"', // 测试服务器
   HTTPCMSURL: '"http://192.168.200.247:30000"', // 请求CMS的接口
-  // HTTPOPENAPIURL: '"http://192.168.200.247:10001"' // 请求OPENAPI的接口
-  HTTPOPENAPIURL: '"http://192.168.202.167:10001"' // 请求OPENAPI的接口
+  HTTPOPENAPIURL: '"http://192.168.200.247:10001"' // 请求OPENAPI的接口
+  // HTTPOPENAPIURL: '"http://192.168.202.167:10001"' // 请求OPENAPI的接口
 });
